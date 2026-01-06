1b3c948ce5a550bf384986ce7d37b0a2b72fe43c
unknown <anhkho881@gmail.com>
2026-01-06 22:05:53 +0700

    change base url

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 import { API_ROOT } from '../utils/constants'
 
 // console.log( process.env.REACT_APP_API_URL)
-const API_BASE_URL = 'https://l-atelia-api.onrender.com'
-// const API_BASE_URL = 'http://localhost:3000'
+// const API_BASE_URL = 'https://l-atelia-api.onrender.com'
+const API_BASE_URL = 'https://l-atelia-api-yct5.onrender.com'
 console.log(API_BASE_URL)
 
 const axiosClient = axios.create({
